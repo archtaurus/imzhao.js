cb393e003b8b88be69b933e104362012e1283216
Zhao Xin <7176466@qq.com>
2021-03-29 18:27:05 +0800

    update

--- bin/imzhao.js
+++ bin/imzhao.js
@@ -4,6 +4,26 @@ const { program } = require('commander')
 const imzhao = require('../lib')
 
 program
+    .command('gcd')
+    .action(() => {
+        const numbers = process.argv.slice(3)
+        if (numbers) {
+            const result = imzhao.math.gcd(...numbers)
+            console.log(result)
+        }
+    })
+
+program
+    .command('lcm')
+    .action(() => {
+        const numbers = process.argv.slice(3)
+        if (numbers) {
+            const result = imzhao.math.lcm(...numbers)
+            console.log(result)
+        }
+    })
+
+program
     .command('publicip')
     .description('Show your public IP address.')
     .action(async () => {

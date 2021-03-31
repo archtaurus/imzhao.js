3d287e1872d12162281f01669a1602e39a4dd1b8
Zhao Xin <7176466@qq.com>
2021-03-31 18:42:23 +0800

    update

--- test/imzhao.net.test.js
+++ test/imzhao.net.test.js
@@ -1,8 +1,34 @@
 const imzhao = require("../lib")
 
 describe("imzhao.net", () => {
+
+    const { isIPv4, getPublicIP } = imzhao.net
     test('getPublicIP() should return IP address', async () => {
-        const result = await imzhao.net.getPublicIP()
-        expect(imzhao.net.isIPv4(result)).toEqual(true)
+        const result = await getPublicIP()
+        expect(isIPv4(result)).toEqual(true)
     })
+
+    const { htmlToText } = imzhao.net
+    test('htmlToText works', () => {
+        const text = 'HELLO,WORLD'
+        const result = htmlToText(`<h1>${text}</h1>`)
+        expect(result).toBe(text)
+    })
+
+    // const { smtp } = imzhao.net
+    // test('sendMail works', (done) => {
+    //     expect(smtp).toBeTruthy()
+    //     expect(process.env.SMTP_MAIL).toBeTruthy()
+    //     const message = {
+    //         from: process.env.SMTP_MAIL,
+    //         to: process.env.SMTP_MAIL,
+    //         subject: 'hello',
+    //         text: 'hello',
+    //     }
+    //     smtp.sendMail(message, (err, info) => {
+    //         expect(info.response.match(/^250 OK/)).toBeTruthy()
+    //         done()
+    //     })
+    // })
+
 })

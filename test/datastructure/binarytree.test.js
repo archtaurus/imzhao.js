ec90b32d6ef0b7834592d1aa366e7b724a845288
Zhao Xin <7176466@qq.com>
2021-04-19 01:31:05 +0800

    update

--- test/datastructure/binarytree.test.js
+++ test/datastructure/binarytree.test.js
@@ -2,11 +2,27 @@ const imzhao = require('imzhao')
 
 describe('imzhao.datastructure.BinaryTree', () => {
     const { BinaryTree } = imzhao.datastructure
-    const tree = new BinaryTree()
 
     test('create', () => {
-        expect(tree.size()).toEqual(0)
+        const tree = new BinaryTree()
+        expect(tree.hasOwnProperty('size')).toBeTruthy()
         expect(tree.hasOwnProperty('isEmpty')).toBeTruthy()
         expect(tree.hasOwnProperty('insert')).toBeTruthy()
+        expect(tree.hasOwnProperty('delete')).toBeTruthy()
+    })
+
+    test('insert', () => {
+        const tree = new BinaryTree()
+        expect(tree.size()).toEqual(0)
+        expect(tree.insert(2)).toBeTruthy()
+        expect(tree.size()).toEqual(1)
+        expect(tree.insert(1)).toBeTruthy()
+        expect(tree.size()).toEqual(2)
+        expect(tree.insert(3)).toBeTruthy()
+        expect(tree.size()).toEqual(3)
+        expect(tree.insert(4)).toBeTruthy()
+        expect(tree.size()).toEqual(4)
+        expect(tree.insert(3)).toBeFalsy()
+        expect(tree.size()).toEqual(4)
     })
 })

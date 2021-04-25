54a18674c61f5c8e18f1561492945c1d36e052bb
Zhao Xin <7176466@qq.com>
2021-04-25 18:14:20 +0800

    update

--- test/algorithm/用栈将十进制转二进制.test.js
+++
@@ -1,7 +0,0 @@
-const { 用栈将十进制转二进制 } = require('../../lib/algorithm')
-
-describe('imzhao.algorithm.用栈将十进制转二进制', () => {
-    test('用栈将十进制转二进制', () => {
-        expect(用栈将十进制转二进制(170)).toEqual('10101010')
-    })
-})

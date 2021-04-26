b428f68494b76906d7e0f5507e7040b050c9690d
Zhao Xin <7176466@qq.com>
2021-04-26 21:15:17 +0800

    add calc24()

--- test/algorithm/calc24.test.js
+++ test/algorithm/calc24.test.js
@@ -3,6 +3,6 @@ const { algorithm } = require('../../lib')
 describe('imzhao.algorithm', () => {
     const { calc24 } = algorithm
     test('calc24', () => {
-        expect(calc24(3, 2, 4, 8).size).toEqual(5)
+        expect(calc24(3, 2, 4, 8).length).toEqual(5)
     })
 })

393721f5bc2af92dc457689c0f7e8da321421010
Zhao Xin <7176466@qq.com>
2021-05-07 15:31:41 +0800

    update

---
+++ test/algorithm/astar.test.js
@@ -0,0 +1,12 @@
+class Node {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+        this.visited = false
+        this.neighbors = []
+    }
+}
+
+describe('astar', () => {
+    test('b', () => {})
+})
